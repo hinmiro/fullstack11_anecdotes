b156a321868b33313821425da9fb6d71f753a6f6
hinmiro <miro.hintikka@gmail.com>
2026-01-20 18:08:40 +0200

    Add server script

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import path from 'path'
+import process from 'process'
+import { createProxyMiddleware } from 'http-proxy-middleware'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.static(path.join(__dirname, 'dist')))
+
+app.use(
+   '/anecdotes',
+   createProxyMiddleware({
+      target: 'http://localhost:4000',
+      changeOrigin: true,
+   })
+)
+
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
+
+app.listen(PORT, () => {
+   console.log(`Server running on port ${PORT}`)
+})
