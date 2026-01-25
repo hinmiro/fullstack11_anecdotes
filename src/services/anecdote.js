43e6c7a13bc2901fff62871f467b9510f8ce8d93
hinmiro <miro.hintikka@gmail.com>
2026-01-25 12:34:16 +0200

    Update server address

--- src/services/anecdote.js
+++ src/services/anecdote.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/anecdotes'
+const baseUrl = import.meta.env.VITE_BACKEND_URL
+
 
 const getAll = async () => {
    const res = await axios.get(baseUrl)
